fc1843511f3c362c009345249047de3911f14561
buludo <farr2008@gmail.com>
2026-07-30 02:44:56 +0800

    first commit

---
+++ src/config/navData.json.ts
@@ -0,0 +1,110 @@
+// utils
+import { countItems, getAllPosts, sortByValue } from "@js/blogUtils";
+import { humanize } from "@js/textUtils";
+
+// get the categories used in blog posts, to put into navbar
+const posts = await getAllPosts();
+const allCategories = posts.map((post) => post.data.categories).flat();
+const countedCategories = countItems(allCategories);
+const processedCategories = sortByValue(countedCategories);
+
+export interface navLinkItem {
+	text: string;
+	link: string;
+	newTab?: boolean; // adds target="_blank" rel="noopener noreferrer" to link
+}
+
+export interface navDropdownItem {
+	text: string;
+	dropdown: navLinkItem[];
+}
+
+export type navItem = navLinkItem | navDropdownItem;
+
+// note: 1 level of dropdown is supported
+const navConfig: navItem[] = [
+	{
+		text: "HOME",
+		link: "/",
+	},
+	// {
+	//	text: "Blog",
+	//	link: "/blog/",
+	//},
+	{
+		text: "CONCERTS & EVENTS",
+		dropdown: [
+			{
+				text: "Home...old",
+				link: "/index3",
+			},
+			{
+				text: "Season 2025",
+				link: "/blog/qinlianqixiang/",
+			},
+			{
+				text: "Season 2024",
+				link: "/blog/melodyforgotten/",
+			},
+			{
+				text: "Season 2023",
+				link: "/categories/2023concerts/",
+			},
+			{
+				text: "Season 2020",
+				link: "/categories/musicians/",
+			},
+			{
+				text: "Concerts",
+				link: "/categories/concerts/",
+			},
+			{
+				text: "Events Timeline",
+				link: "/eventstimeline",
+			},
+		],
+	},
+	{
+		text: "ABOUT",
+		dropdown: [
+			{
+				text: "Orchestra",
+				link: "/blog/orchestra/",
+			},
+			{
+				text: "Music Director",
+				link: "/blog/yichia-lai/",
+			},
+			{
+				text: "Concert Master",
+				link: "/blog/tyler-wang/",
+			},
+			{
+				text: "Musicians",
+				link: "#",
+			},
+			{
+				text: "Strings",
+				link: "/categories/strings/",
+			},
+			{
+				text: "Woodwinds",
+				link: "/categories/woodwinds/",
+			},
+			{
+				text: "Brass & Percussion",
+				link: "/categories/brass-percussion/",
+			},
+			{
+				text: "Contact",
+				link: "/contact/",
+			},
+		],
+	},
+	{
+		text: "SUPPORT",
+		link: "/blog/support/",
+	},
+];
+
+export default navConfig;
---
+++ src/config/siteData.json.ts
@@ -0,0 +1,38 @@
+export interface SiteDataProps {
+	name: string;
+	title: string;
+	description: string;
+	useViewTransitions?: boolean; // defaults to false. Set to true to enable some Astro 3.0 view transitions
+	author: {
+		name: string;
+		email: string;
+		twitter: string; // used for twitter cards when sharing a blog post on twitter
+	};
+	defaultImage: {
+		src: string;
+		alt: string;
+	};
+}
+
+// Update this file with your site specific information
+const siteData: SiteDataProps = {
+	name: "台北城市愛樂",
+	// Your website's title and description (meta fields)
+	title: "台北城市愛樂 Taipei City Philiharmonic",
+	description: "台北城市愛樂 Taipei City Philiharmonic",
+	useViewTransitions: true,
+	// Your information!
+	author: {
+		name: "台北城市愛樂 Taipei City Philiharmonic",
+		email: "tcph2026@hotmail.com",
+		twitter: "TCPH_2026",
+	},
+
+	// default image for meta tags if the page doesn't have an image already
+	defaultImage: {
+		src: "/images/site-logo.png",
+		alt: "TCPH logo",
+	},
+};
+
+export default siteData;
