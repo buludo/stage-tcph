bbd5e7624f0b6885276c7dcd5872c974fb853ebf
Richard <farr2008@gmail.com>
2026-08-13 15:11:05 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://buludo.github.io",
-    base: "/stage-tcph",
+    site: "https://tcph.org.tw",
+    // base: "/stage-tcph",
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
